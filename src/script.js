a121c0851a384e0b8785a2ac98d2dfdf39090e02
sampath88 <sampathreddy8896@gmail.com>
2024-07-18 19:23:32 +0530

    fullscreen and resizing

--- src/script.js
+++ src/script.js
@@ -1,4 +1,43 @@
+import "./style.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  //update renderer
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+window.addEventListener("dblclick", () => {
+  let fullscreenElement =
+    document.fullscreenElement || document.webkitFullscreenElement;
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen();
+    } else if (canvas.webkitFullscreenElement) {
+      canvas.webkitFullscreenElement();
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+});
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -18,19 +57,22 @@ const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
 /**
- * Sizes
- */
-const sizes = {
-  width: 800,
-  height: 600,
-};
-
-/**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+const camera = new THREE.PerspectiveCamera(
+  /* Field of view: */ 75,
+  /* Aspect Ratio: */ sizes.width / sizes.height,
+  /* Near: */ 0.1,
+  /* Far: */ 100
+);
+
 camera.position.z = 3;
+camera.lookAt(mesh.position);
 scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
@@ -39,4 +81,15 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+const tick = () => {
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+
+tick();
